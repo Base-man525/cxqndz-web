0079abb2dde25e57a3296b6da8b1b4ebdae9159e
Base-man525 <baseeric3@gmail.com>
2026-07-03 10:38:24 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.cxqndz.com',
+  site: 'https://base-man525.github.io',
+  base: '/cxqndz-web/',
+  outDir: './dist',
   output: 'static',
   build: {
     assets: 'assets',
